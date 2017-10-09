8882af4d847fbc7a509ad89035d1c3c63730600c
Catarina Ferreira <up201506671@fe.up.pt>
2017-10-09 19:22:38 +0100

    writenoncanonical feito (acho eu)

--- tp2/writenoncanonical.c
+++ tp2/writenoncanonical.c
@@ -14,107 +14,176 @@
 #define FALSE 0
 #define TRUE 1
 
+////////////////////////////////////////////////////
+#define FLAG 0x7E
+#define A 0X03
+#define setC 0X03
+#define uaC 0X07
+
 volatile int STOP=FALSE;
 
+
+int sumAlarms=0;
+int connectionEstabilished=0;
+
+unsigned char SET[5];
+SET[0] = FLAG;
+SET[1] = A;
+SET[2] = setC;
+SET[3]= SET[1]^SET[2];
+SET[4] = FLAG;
+
+void alarmTimeout(){
+
+  printf("Alarm=%d", sumAlarms);
+
+  //se ainda nao tivermos recebido UA e a soma dos alarmes for inferior a 4
+  if(!connectionEstabilished && sumAlarms < 4 )
+  {
+    sumAlarms++;
+    requestConnection();
+  }
+  else
+  {
+    printf("O pedido falhou!\n");
+    exit(-1);
+  }
+
+}
 int main(int argc, char** argv)
 {
-    int fd,c, res;
-    struct termios oldtio,newtio;
-    char buf[255];
-    int i, sum = 0, speed = 0;
+  int fd,c, res;
+  struct termios oldtio,newtio;
+  char buf[255];
+  int i, sum = 0, speed = 0;
 
-    if ( (argc < 2) ||
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) &&
-  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-      exit(1);
-    }
-
-
-  /*
-    Open serial port device for reading and writing and not as controlling tty
-    because we don't want to get killed if linenoise sends CTRL-C.
-  */
-
-
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(argv[1]); exit(-1); }
-
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-      perror("tcgetattr");
-      exit(-1);
-    }
-
-    bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
-
-    /* set input mode (non-canonical, no echo,...) */
-    newtio.c_lflag = 0;
-
-    newtio.c_cc[VTIME]    = 1;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 0;   /* blocking read until 5 chars received */
-
-
+  if ( (argc < 2) ||
+  ((strcmp("/dev/ttyS0", argv[1])!=0) &&
+  (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+    printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+    exit(1);
+  }
 
   /*
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
-    leitura do(s) pr�ximo(s) caracter(es)
+  Open serial port device for reading and writing and not as controlling tty
+  because we don't want to get killed if linenoise sends CTRL-C.
   */
+  fd = open(argv[1], O_RDWR | O_NOCTTY );
+  if (fd <0) {perror(argv[1]); exit(-1); }
 
+  if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+    perror("tcgetattr");
+    exit(-1);
+  }
 
+  bzero(&newtio, sizeof(newtio));
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
 
-    tcflush(fd, TCIOFLUSH);
+  /* set input mode (non-canonical, no echo,...) */
+  newtio.c_lflag = 0;
 
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
-
-    printf("New termios structure set\n");
-
-    gets(buf);
-
-    printf("%s\n",buf);
-    //for (i = 0; i < sizeof(buf); i++) {
-      //buf[i] = 'a';
-    res=write(fd, buf, strlen(buf)+1);
-    //}
-
-    /*testing*/
-    //buf[25] = '\n';
-
-    //res = write(fd,buf,255);
-    printf("%d bytes written\n", res);
-
+  newtio.c_cc[VTIME]    = 1;   /* inter-character timer unused */
+  newtio.c_cc[VMIN]     = 0;   /* blocking read until 5 chars received */
 
   /*
-    O ciclo FOR e as instru��es seguintes devem ser alterados de modo a respeitar
-    o indicado no gui�o
+  VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+  leitura do(s) pr�ximo(s) caracter(es)
   */
 
+  tcflush(fd, TCIOFLUSH);
+
+  if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
+
+  printf("New termios structure set\n");
+
+  // instalar handler do alarme
+  (void) signal(SIGALRM,alarmTimeout);
+
+  LLOPEN(fd,0);
+
+  sleep(1);
+  if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
+
+  close(fd);
+  return 0;
+}
 
 
+unsigned char UA[5];
+UA[0]=FLAG;
+UA[1]=A;
+UA[2]=uaC;
+UA[3]=A^uaC;
+UA[4]=FLAG;
 
-    int j = 0;
-    while (STOP==FALSE) {       /* loop for input */
-      res = read(fd,buf+j,1);   /* returns after 5 chars have been input */
 
-      if(res == 1){
-          if (buf[j]=='\0') STOP=TRUE;
-        j++;
-      }
+void LLOPEN(int fd, int x){
+  requestConnection();
+  responseRequestConnection();
+}
+
+//manda a trama SET
+void requestConnection(){
+  write(fd,SET,5);
+  alarm(3);
+}
+
+void responseRequestConnection(){
+  unsigned char c;
+  int state=0;
+  while(!connectionEstabilished){
+    read(fd,&c,1);
+    switch(state){
+      //recebe flag
+      case 0:
+        if(c==UA[0])
+          state=1;
+          break;
+      //recebe A
+      case 1:
+        if(c==UA[1])
+          state=2;
+        else
+          {
+            if(c==UA[0])
+              state=1;
+            else
+              state = 0;
+          }
+      break;
+      //recebe C
+      case 2:
+        if(c==UA[2])
+          state=3;
+        else{
+          if(c==UA[0])
+            state=1;
+          else
+            state = 0;
+        }
+      break;
+      //recebe BCC
+      case 3:
+        if(c==UA[3])
+          state = 4;
+        else
+          state=0;
+      break;
+      //recebe FLAG final
+      case 4:
+        if(c==UA[4])
+          connectionEstabilished=1;
+        else
+          state = 0;
+      break;
     }
-
-    printf("RECETOR: %s ; %d\n", buf,j );
-
-    sleep(1);
-    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
-
-    close(fd);
-    return 0;
+  }
 }
