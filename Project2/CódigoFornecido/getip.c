847ad98506b41e8d99013f9a7b8e1299a69264f1
Sofia Silva <up201505628@fe.up.pt>
2017-12-01 10:42:32 +0000

    started report

--- Project2/CódigoFornecido/getip.c
+++ Project2/CódigoFornecido/getip.c
@@ -1,22 +1,22 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <errno.h> 
-#include <netdb.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <netdb.h>
 #include <sys/types.h>
-#include <netinet/in.h> 
-#include<arpa/inet.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main(int argc, char *argv[])
 {
-	struct hostent *h;
+    struct hostent *h;
 
-        if (argc != 2) {  
-            fprintf(stderr,"usage: getip address\n");
-            exit(1);
-        }
-        
-        
-/*
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: getip address\n");
+        exit(1);
+    }
+
+    /*
 struct hostent {
 	char    *h_name;	Official name of the host. 
     	char    **h_aliases;	A NULL-terminated array of alternate names for the host. 
@@ -28,13 +28,14 @@ struct hostent {
 
 #define h_addr h_addr_list[0]	The first address in h_addr_list. 
 */
-        if ((h=gethostbyname(argv[1])) == NULL) {  
-            herror("gethostbyname");
-            exit(1);
-        }
+    if ((h = gethostbyname(argv[1])) == NULL)
+    {
+        herror("gethostbyname");
+        exit(1);
+    }
 
-        printf("Host name  : %s\n", h->h_name);
-        printf("IP Address : %s\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
+    printf("Host name  : %s\n", h->h_name);
+    printf("IP Address : %s\n", inet_ntoa(*((struct in_addr *)h->h_addr)));
 
-        return 0;
+    return 0;
 }
--- Project2/clientTCP.c
+++ Project2/clientTCP.c
@@ -16,20 +16,21 @@
 #define SERVER_ADDR "192.168.28.96"
 #define MAX_STRING_LENGTH 50
 
-void readResponse(int socketfd, char* responseCode);
+void readResponse(int socketfd, char *responseCode);
 struct hostent *getip(char host[]);
 void createFile(unsigned char *mensagem, off_t *sizeFile, unsigned char filename[]);
-void parseArgument(char* argument,char* user,char* pass,char* host,char* path);
-int sendCommandInterpretResponse(int socketfd,char cmd[],char commandContent[]);
+void parseArgument(char *argument, char *user, char *pass, char *host, char *path);
+int sendCommandInterpretResponse(int socketfd, char cmd[], char commandContent[]);
 int getServerPortFromResponse(int socketfd);
 
-int main(int argc, char** argv){
-	int	socketfd;
-	int	socketfdClient;
-	struct	sockaddr_in server_addr;
-	struct	sockaddr_in server_addr_client;
-	char	buf[] = "";
-	int	bytes;
+int main(int argc, char **argv)
+{
+	int socketfd;
+	int socketfdClient;
+	struct sockaddr_in server_addr;
+	struct sockaddr_in server_addr_client;
+	char buf[] = "";
+	int bytes;
 
 	struct hostent *h;
 
@@ -45,125 +46,141 @@ int main(int argc, char** argv){
 
 	char path[MAX_STRING_LENGTH];
 	memset(path, 0, MAX_STRING_LENGTH);
-	parseArgument(argv[1],user,pass,host,path);
+	parseArgument(argv[1], user, pass, host, path);
 
-	printf("user %s\n",user);
-	printf("pass %s\n",pass);
-	printf("host %s\n",host);
-	printf("path %s\n",path);
+	printf("user %s\n", user);
+	printf("pass %s\n", pass);
+	printf("host %s\n", host);
+	printf("path %s\n", path);
 
 	h = getip(host);
 
-	printf("IP Address : %s\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
-
+	printf("IP Address : %s\n", inet_ntoa(*((struct in_addr *)h->h_addr)));
 
 	/*server address handling*/
-	bzero((char*)&server_addr,sizeof(server_addr));
+	bzero((char *)&server_addr, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)h->h_addr)));	/*32 bit Internet address network byte ordered*/
-	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
+	server_addr.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)h->h_addr))); /*32 bit Internet address network byte ordered*/
+	server_addr.sin_port = htons(SERVER_PORT);											/*server TCP port must be network byte ordered */
 
 	/*open an TCP socket*/
-	if ((socketfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+	if ((socketfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
 		perror("socket()");
 		exit(0);
 	}
 	/*connect to the server*/
-	if(connect(socketfd,(struct sockaddr *)&server_addr,sizeof(server_addr)) < 0){
-			perror("connect()");
-			exit(0);
-		}
+	if (connect(socketfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+	{
+		perror("connect()");
+		exit(0);
+	}
 
 	////////////////////////////////////////////
 	//Aqui so temos em conta se a responsta foi
 	//positiva, nao sei como fazer se nao for //
-	readResponse(socketfd,responseCode);	//
-	if(responseCode[0]=='2'){											//
-		printf("Connection Estabilished\n");		//
-	}																					//
+	readResponse(socketfd, responseCode); //
+	if (responseCode[0] == '2')
+	{										 //
+		printf("Connection Estabilished\n"); //
+	}										 //
 	////////////////////////////////////////////
 
 	printf("Sending Username\n");
-	int res = sendCommandInterpretResponse(socketfd,"user ",user);
-	if(res == 1){
+	int res = sendCommandInterpretResponse(socketfd, "user ", user);
+	if (res == 1)
+	{
 		printf("Sending Password\n");
-		res = sendCommandInterpretResponse(socketfd,"pass ",pass);
+		res = sendCommandInterpretResponse(socketfd, "pass ", pass);
 	}
 
-	write(socketfd,"pasv\n",5);
+	write(socketfd, "pasv\n", 5);
 	int serverPort = getServerPortFromResponse(socketfd);
 
 	/*server address handling*/
-	bzero((char*)&server_addr_client,sizeof(server_addr_client));
+	bzero((char *)&server_addr_client, sizeof(server_addr_client));
 	server_addr_client.sin_family = AF_INET;
-	server_addr_client.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)h->h_addr)));	/*32 bit Internet address network byte ordered*/
-	server_addr_client.sin_port = htons(serverPort);		/*server TCP port must be network byte ordered */
+	server_addr_client.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)h->h_addr))); /*32 bit Internet address network byte ordered*/
+	server_addr_client.sin_port = htons(serverPort);										   /*server TCP port must be network byte ordered */
 
 	/*open an TCP socket*/
-	if ((socketfdClient = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+	if ((socketfdClient = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
 		perror("socket()");
 		exit(0);
 	}
 	/*connect to the server*/
-	if(connect(socketfdClient,(struct sockaddr *)&server_addr_client,sizeof(server_addr_client)) < 0){
-			perror("connect()");
-			exit(0);
-		}
-		printf("\nSending Retr\n");
-		sendCommandInterpretResponse(socketfd,"retr ",path);
+	if (connect(socketfdClient, (struct sockaddr *)&server_addr_client, sizeof(server_addr_client)) < 0)
+	{
+		perror("connect()");
+		exit(0);
+	}
+	printf("\nSending Retr\n");
+	sendCommandInterpretResponse(socketfd, "retr ", path);
 
 	close(socketfd);
 	exit(0);
 }
 
-
-
-	// ./download ftp://anonymous:1@speedtest.tele2.net/1KB.zip
-void parseArgument(char* argument,char* user,char* pass,char* host,char* path) {
+// ./download ftp://anonymous:1@speedtest.tele2.net/1KB.zip
+void parseArgument(char *argument, char *user, char *pass, char *host, char *path)
+{
 	char start[] = "ftp://";
 	int index = 0;
 	int i = 0;
 	int state = 0;
 	int length = strlen(argument);
-	while (i < length) {
-		switch (state) {
-			case 0://reads the ftp://
-			if (argument[i] == start[i] && i < 5) {
+	while (i < length)
+	{
+		switch (state)
+		{
+		case 0: //reads the ftp://
+			if (argument[i] == start[i] && i < 5)
+			{
 				break;
 			}
-			if(i == 5 && argument[i] == start[i])
-			state = 1;
+			if (i == 5 && argument[i] == start[i])
+				state = 1;
 			else
-			printf("Error parsing ftp://");
+				printf("Error parsing ftp://");
 			break;
-			case 1://reads the username
-			if(argument[i] == ':'){
+		case 1: //reads the username
+			if (argument[i] == ':')
+			{
 				state = 2;
 				index = 0;
-			}else{
+			}
+			else
+			{
 				user[index] = argument[i];
 				index++;
 			}
 			break;
-			case 2:
-			if (argument[i] == '@') {
+		case 2:
+			if (argument[i] == '@')
+			{
 				state = 3;
 				index = 0;
-			}else{
+			}
+			else
+			{
 				pass[index] = argument[i];
 				index++;
 			}
 			break;
-			case 3:
-			if (argument[i] == '/') {
+		case 3:
+			if (argument[i] == '/')
+			{
 				state = 4;
 				index = 0;
-			}else{
+			}
+			else
+			{
 				host[index] = argument[i];
 				index++;
 			}
 			break;
-			case 4:
+		case 4:
 			path[index] = argument[i];
 			index++;
 			break;
@@ -173,10 +190,12 @@ void parseArgument(char* argument,char* user,char* pass,char* host,char* path) {
 }
 
 //gets ip address according to the host's name
-struct hostent * getip(char host[]){
+struct hostent *getip(char host[])
+{
 	struct hostent *h;
 
-	if ((h=gethostbyname(host)) == NULL) {
+	if ((h = gethostbyname(host)) == NULL)
+	{
 		herror("gethostbyname");
 		exit(1);
 	}
@@ -185,110 +204,138 @@ struct hostent * getip(char host[]){
 }
 
 //reads response code from the server
-void readResponse(int socketfd, char* responseCode){
+void readResponse(int socketfd, char *responseCode)
+{
 	int state = 0;
 	int index = 0;
 	char c;
 
-	while (state != 3) {
-		read(socketfd,&c,1);
-		printf("index %d  char %c \n",index,c);
-		switch (state) {
-			//waits for 3 digit number followed by ' ' or '-'
-			case 0:
-				if(c == ' '){
-					if (index != 3) {
-						printf("Error receiving response code\n");
-						return;
+	while (state != 3)
+	{
+		read(socketfd, &c, 1);
+		printf("index %d  char %c \n", index, c);
+		switch (state)
+		{
+		//waits for 3 digit number followed by ' ' or '-'
+		case 0:
+			if (c == ' ')
+			{
+				if (index != 3)
+				{
+					printf("Error receiving response code\n");
+					return;
+				}
+				index = 0;
+				state = 1;
+			}
+			else
+			{
+				if (c == '-')
+				{
+					state = 2;
+				}
+				else
+				{
+					if (isdigit(c))
+					{
+						responseCode[index] = c;
+						index++;
 					}
-					index = 0;
+				}
+			}
+			break;
+		//reads until the end of the line
+		case 1:
+			if (c == '\n')
+			{
+				state = 3;
+			}
+			break;
+		//waits for response code in multiple line responses
+		case 2:
+			if (c == responseCode[index])
+			{
+				index++;
+			}
+			else
+			{
+				if (index == 3 && c == ' ')
+				{
 					state = 1;
-				}else{
-					if (c == '-') {
-						state = 2;
-					}
-					else{
-						if(isdigit(c)){
-							responseCode[index] = c;
-							index++;
-						}
-					}
 				}
-				break;
-			//reads until the end of the line
-			case 1:
-				if(c == '\n'){
-					state = 3;
+				else
+				{
+					printf("Error receiving response code\n");
+					return;
 				}
-				break;
-			//waits for response code in multiple line responses
-			case 2:
-				if(c == responseCode[index]){
-					index++;
-				}else{
-					if(index == 3 && c == ' '){
-						state = 1;
-					}else {
-						printf("Error receiving response code\n");
-						return;
-					}
-				}
-				break;
+			}
+			break;
 		}
 	}
 }
 
 //reads the server port when pasv is sent
-int getServerPortFromResponse(int socketfd){
+int getServerPortFromResponse(int socketfd)
+{
 	int state = 0;
 	int index = 0;
-	char firstByte [4];
-	memset(firstByte, 0,4);
-	char secondByte [4];
-	memset(secondByte, 0,4);
+	char firstByte[4];
+	memset(firstByte, 0, 4);
+	char secondByte[4];
+	memset(secondByte, 0, 4);
 
 	char c;
 
-	while (state != 7) {
-		read(socketfd,&c,1);
-		printf("%c",c);
-		switch (state) {
-			//waits for 3 digit number followed by ' '
-			case 0:
-				if(c == ' '){
-					if (index != 3) {
-						printf("Error receiving response code\n");
-						return -1;
-					}
-					index = 0;
-					state = 1;
+	while (state != 7)
+	{
+		read(socketfd, &c, 1);
+		printf("%c", c);
+		switch (state)
+		{
+		//waits for 3 digit number followed by ' '
+		case 0:
+			if (c == ' ')
+			{
+				if (index != 3)
+				{
+					printf("Error receiving response code\n");
+					return -1;
 				}
-				else{
-					index++;
-				}
-				break;
-			case 5:
-				if(c == ','){
-					index = 0;
-					state++;
-				}
-				else{
-					firstByte[index]=c;
-					index++;
-				}
-				break;
-			case 6:
-				if(c == ')'){
-					state++;
-				}
-				else{
-					secondByte[index]=c;
-					index++;
-				}
-				break;
-			//reads until the first comma
-			default:
-			if(c == ','){
+				index = 0;
+				state = 1;
+			}
+			else
+			{
+				index++;
+			}
+			break;
+		case 5:
+			if (c == ',')
+			{
+				index = 0;
+				state++;
+			}
+			else
+			{
+				firstByte[index] = c;
+				index++;
+			}
+			break;
+		case 6:
+			if (c == ')')
+			{
+				state++;
+			}
+			else
+			{
+				secondByte[index] = c;
+				index++;
+			}
+			break;
+		//reads until the first comma
+		default:
+			if (c == ',')
+			{
 				state++;
 			}
 			break;
@@ -297,49 +344,53 @@ int getServerPortFromResponse(int socketfd){
 
 	int firstByteInt = atoi(firstByte);
 	int secondByteInt = atoi(secondByte);
-	return (firstByteInt*256 + secondByteInt);
+	return (firstByteInt * 256 + secondByteInt);
 }
 
 //sends a command, reads the response from the server and interprets it
-int sendCommandInterpretResponse(int socketfd,char cmd[],char commandContent[]){
+int sendCommandInterpretResponse(int socketfd, char cmd[], char commandContent[])
+{
 	char responseCode[3];
 	int action = 0;
 	//sends the command
-	write(socketfd,cmd,strlen(cmd));
-	write(socketfd,commandContent,strlen(commandContent));
-	write(socketfd,"\n",1);
+	write(socketfd, cmd, strlen(cmd));
+	write(socketfd, commandContent, strlen(commandContent));
+	write(socketfd, "\n", 1);
 
-	while(1){
+	while (1)
+	{
 		//reads the response
-	 	readResponse(socketfd,responseCode);
+		readResponse(socketfd, responseCode);
 		action = responseCode[0] - '0';
 
-		switch (action) {
-			//waits for another response
-			case 1:
-				readResponse(socketfd,responseCode);
-				break;
-			//command accepted, we can send another command
-			case 2:
-				return 0;
-			//needs additional information
-			case 3:
-				return 1;
-			//try again
-			case 4:
-				write(socketfd,cmd,strlen(cmd));
-				write(socketfd,commandContent,strlen(commandContent));
-				write(socketfd,"\n",1);
-				break;
-			case 5:
-				printf("Command wasn\'t accepted!\n Goodbye");
-				close(socketfd);
-				exit(-1);
+		switch (action)
+		{
+		//waits for another response
+		case 1:
+			readResponse(socketfd, responseCode);
+			break;
+		//command accepted, we can send another command
+		case 2:
+			return 0;
+		//needs additional information
+		case 3:
+			return 1;
+		//try again
+		case 4:
+			write(socketfd, cmd, strlen(cmd));
+			write(socketfd, commandContent, strlen(commandContent));
+			write(socketfd, "\n", 1);
+			break;
+		case 5:
+			printf("Command wasn\'t accepted!\n Goodbye");
+			close(socketfd);
+			exit(-1);
 		}
 	}
 }
 
-void createFile(unsigned char *mensagem, off_t *sizeFile, unsigned char filename[]){
+void createFile(unsigned char *mensagem, off_t *sizeFile, unsigned char filename[])
+{
 	FILE *file = fopen((char *)filename, "wb+");
 	fwrite((void *)mensagem, 1, *sizeFile, file);
 	printf("%zd\n", *sizeFile);
